45295a34a4f2889fa5398ba2529d32598e42087e
Afif Akromi <massachusetts48@gmail.com>
2022-06-03 01:19:01 +0700

    fetch to weather API

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { Platform, Text, View, StyleSheet } from "react-native";
 import * as Location from "expo-location";
+import axios from "axios";
 
 const WEATHER_API_KEY = "5dab8f9388bca535acf0e30a76325173";
+const WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/weather?";
 
 export default function App() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+  const [location, setLocation] = React.useState(null);
+  const [errorMsg, setErrorMsg] = React.useState(null);
+  const [currentWeather, setCurrentWeather] = React.useState(null);
 
   Location.getCurrentPositionAsync({
     accuracy: Location.Accuracy.Highest,
@@ -26,6 +29,16 @@ export default function App() {
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+
+      const weatherUrl = `${WEATHER_BASE_URL}lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${WEATHER_API_KEY}`;
+      const response = await fetch(weatherUrl);
+      const result = await response.json();
+
+      if (response.ok) {
+        setCurrentWeather(result);
+      } else {
+        setErrorMsg(result.message);
+      }
     })();
   }, []);
 
