ed5b604888ac6e8c2813b3bc0c51f3a254d6f317
Afif Akromi <massachusetts48@gmail.com>
2022-06-07 11:04:37 +0700

    make API Call

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { Platform, Text, View, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import { Text, View, StyleSheet } from "react-native";
 import * as Location from "expo-location";
-import axios from "axios";
+import WeatherInfo from "./src/components/WeatherInfo";
 
 const WEATHER_API_KEY = "5dab8f9388bca535acf0e30a76325173";
 const WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/weather?";
@@ -10,6 +10,7 @@ export default function App() {
   const [location, setLocation] = React.useState(null);
   const [errorMsg, setErrorMsg] = React.useState(null);
   const [currentWeather, setCurrentWeather] = React.useState(null);
+  const [unitSystem, setUnitSystem] = React.useState("metric");
 
   Location.getCurrentPositionAsync({
     accuracy: Location.Accuracy.Highest,
@@ -30,7 +31,7 @@ export default function App() {
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
 
-      const weatherUrl = `${WEATHER_BASE_URL}lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${WEATHER_API_KEY}`;
+      const weatherUrl = `${WEATHER_BASE_URL}lat=${location.coords.latitude}&lon=${location.coords.longitude}&units=${unitSystem}&appid=${WEATHER_API_KEY}`;
       const response = await fetch(weatherUrl);
       const result = await response.json();
 
@@ -49,18 +50,31 @@ export default function App() {
     text = JSON.stringify(location.coords.latitude);
   }
 
-  return (
-    <View style={styles.container}>
-      <Text>{text}</Text>
-    </View>
-  );
+  if (currentWeather) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.main}>
+          <WeatherInfo currentWeather={currentWeather} />
+        </View>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>{text}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+  },
+
+  main: {
+    justifyContent: "center",
+    flex: 1,
   },
 });
